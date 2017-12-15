fdce8746d747a2d1ad93ce23e8f3b00bcfc65666
Joshua Magady <josh.magady@gmail.com>
2017-12-14 21:25:55 -0600

    feat(*): Updated files
    ITVictim_audio.zip needs to be unzipped before starting the project. The file size is larger then github allows

--- Assets/Scripts/VideoControler.cs
+++ Assets/Scripts/VideoControler.cs
@@ -3,75 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class VideoControler : MonoBehaviour {
+public class VideoControler : MonoBehaviour
+{
 
     private UnityEngine.Video.VideoPlayer videoPlayer;
     public GameObject pauseSplash; //splash screen that displays Pause Gui
     public GameObject choiceSplash; //splash screen that displays choice Gui
-    public float vidtimeupperband; // Upper Timer Band
-    public float vidtimelowerband; // Lower Timer Band
-    public float vidmidband; // Middle(ish) Timer Band
-    private float count = 2f;
 
-    [SerializeField]
-    private AudioSource audioSource;
+    //[SerializeField]
+    //private AudioSource audioSource;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.loopPointReached += EndReached;
 
-        if (videoPlayer.clip != null) // if there is a video clip, enable
-        {
-            videoPlayer.EnableAudioTrack(0, true);
-            videoPlayer.SetTargetAudioSource(0, audioSource);
-        }
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         //Play or pause the video from user input
-        if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0) && (videoPlayer.time <= vidtimelowerband || videoPlayer.time >= vidtimeupperband))
+        if (Input.GetKeyDown("space") ||
+            Input.GetMouseButtonDown(0))
         {
             if (videoPlayer.isPlaying)
             {
                 videoPlayer.Pause();
-                audioSource.Pause();
                 pauseSplash.SetActive(true);
             }
 
             else
             {
                 videoPlayer.Play();
-                audioSource.Play();
                 pauseSplash.SetActive(false);
             }
         }
-
-        else if (Input.GetMouseButton(0))
-        {
-            count -= Time.deltaTime;
-            if (count < 0)
-            {
-                SceneManager.LoadScene("WaterfallAdventure");
-                count = 2f;
-            }
-
-        }
-
-        else if (Input.GetMouseButtonUp(0))
-        {
-            count = 2f;
-        }
-
-        //Debug.Log("count: " + count);
-
-        if (videoPlayer.time >= vidmidband)
-        {
-            videoPlayer.Pause();
-            audioSource.Pause();
-            choiceSplash.SetActive(true);
-        }
     }
 
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        vp.Pause();
+        choiceSplash.SetActive(true);
+    }
 }
 
